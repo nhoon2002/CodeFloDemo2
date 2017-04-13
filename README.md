155a4c65274934f9f5978e9e641bf4a98cd63cd4
NH\nhoon_000 <nhoon2002@ucla.edu>
2017-04-13 04:21:25 -0700

    edits

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # CodeFlo
+----------
+CodeFlo is a full-stack social networking application, built from scratch by developers [Alex Nam Kim](https://github.com/nhoon2002) & [Henry Lee](https://github.com/henhen87). This project was our ambitious attempt at a MERN stack social networking service, complete with rich features such as real-time chat, team builder, todo list, etc.
+
+Our goal was to build a platform for all individuals who have a great app idea, but do not have the man power or expertise to make it come to fruition. CodeFlo hopes to make it easier for ideas to flow and keep flo'ing.
+
+
+[Take a look at the demo!](https://sketch-that.herokuapp.com/)
+
+----------
+
+
+
+----------
+**Technologies**
+
+ - MongoDB, Express.js, React.js (w/ Redux), Node.js [MERN], MySQL, Socket.IO, Passport.js
+
+
+----------
+## Getting started
+0. Make sure you have MongoDB and mySQL installed before continuing.
+1. Create a mySQL database named 'user_db' and use it with the following queries.
+```
+CREATE DATABASE user_db;
+USE user_db;
+```
+2. Open console and run:
+```
+npm install
+
+node server.js
+```
+3. Open another console and run:
+```
+webpack -w
+```
+4. Head to localhost:3000/
+----------
+### Authors
+- [Alex Nam Kim](https://github.com/nhoon2002)
+- [Henry Lee](https://github.com/henhen87)
--- src/components/MainFooter.jsx
+++ src/components/MainFooter.jsx
@@ -16,21 +16,21 @@ class MainFooter extends React.Component {
       <footer className='Footer'>
         <ul className="Footer-links">
           <li>
-            <a target='_blank' href="http://github.com/nhoon2002/CodeFloDemo2">Github Repository</a>
+            <a target='_blank' href="http://github.com/nhoon2002/CodeFloDemo2">|  Github Repository   |</a>
           </li>
 
           <li>
-            <a target='_blank' href="http://github.com/nhoon2002">Alex Github</a>
+            <a target='_blank' href="http://github.com/nhoon2002">|  Alex Github | </a>
           </li>
           <li>
-            <a target='_blank' href="https://www.linkedin.com/in/alex-nam-kim-04388070/">Alex LinkedIn</a>
+            <a target='_blank' href="https://www.linkedin.com/in/alex-nam-kim-04388070/">|   Alex LinkedIn   | </a>
           </li>
 
           <li>
-            <a target='_blank' href="http://github.com/henhen87">Henry Github</a>
+            <a target='_blank' href="http://github.com/henhen87">|   Henry Github | </a>
           </li>
           <li>
-            <a target='_blank' href="https://www.linkedin.com/in/henry-lee-77170b78/">Henry LinkedIn</a>
+            <a target='_blank' href="https://www.linkedin.com/in/henry-lee-77170b78/">|   Henry LinkedIn  | </a>
           </li>
           <li><h4>CodeFlo&copy; 2017</h4></li>
         </ul>
